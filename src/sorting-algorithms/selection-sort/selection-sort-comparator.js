6aad650d45c6a8aaa561026385046ea4683713ae
NKaty <nd.katy@gmail.com>
2019-01-22 20:15:31 +0300

    Add selection sort

---
+++ src/sorting-algorithms/selection-sort/selection-sort-comparator.js
@@ -0,0 +1,68 @@
+// Selection Sort Comparator
+// Implement a function called selectionSort.
+// Given an array, selectionSort will sort the values in the array.
+// The function takes 2 parameters: an array and an optional comparator function.
+// The comparator function is a callback that will take two values from the array to be compared.
+// The function returns a negative value if the first value is less than the second,
+// a positive value if the first value is greater than the second,
+// and 0 if both values are equal.
+//
+// The default comparator you provide should assume that the two parameters are
+// numbers and that we are sorting the values from smallest to largest.
+
+function selectionSort(arr, comparator) {
+  if (typeof comparator !== 'function') {
+    comparator = (a, b) => {
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    };
+  }
+
+  let min;
+
+  for (let i = 0; i < arr.length; i++) {
+    min = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (comparator(arr[min], arr[j]) > 0) min = j;
+    }
+
+    if (min !== i) [arr[i], arr[min]] = [arr[min], arr[i]];
+  }
+
+  return arr;
+}
+
+const nums = [4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32];
+console.log(selectionSort(nums)); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67, 75, 232, 232, 453, 546, 4342]
+
+console.log(selectionSort(['LilBub', 'Garfield', 'Blue', 'Grumpy'], (a, b) => {
+  if (a[1] < b[1]) return -1;
+  if (a[1] > b[1]) return 1;
+  return 0;
+})); // [ 'Garfield', 'LilBub', 'Blue', 'Grumpy' ]
+
+const moarKittyData = [{
+  name: 'LilBub',
+  age: 7
+}, {
+  name: 'Garfield',
+  age: 40
+}, {
+  name: 'Heathcliff',
+  age: 45
+}, {
+  name: 'Blue',
+  age: 1
+}, {
+  name: 'Grumpy',
+  age: 6
+}];
+
+console.log(selectionSort(moarKittyData, (a, b) => b.age - a.age)); // sorted by age in descending order
+// [ { name: 'Heathcliff', age: 45 },
+//   { name: 'Garfield', age: 40 },
+//   { name: 'LilBub', age: 7 },
+//   { name: 'Grumpy', age: 6 },
+//   { name: 'Blue', age: 1 } ]
---
+++ src/sorting-algorithms/selection-sort/selection-sort.js
@@ -0,0 +1,21 @@
+// Selection Sort
+// Selection sort is an O(n^2) algorithm.
+
+function selectionSort(arr) {
+  let min;
+
+  for (let i = 0; i < arr.length; i++) {
+    min = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) min = j;
+    }
+    if (min !== i) [arr[i], arr[min]] = [arr[min], arr[i]];
+  }
+
+  return arr;
+}
+
+let nums = [4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32];
+console.log(selectionSort(nums)); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67, 75, 232, 232, 453, 546, 4342]
+console.log(selectionSort([0, -10, 7, 4])); // [-10, 0, 4, 7]
