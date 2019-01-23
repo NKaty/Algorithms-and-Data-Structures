cd50b20155e4ff870935435c99e421210d9ab4b6
NKaty <nd.katy@gmail.com>
2019-01-23 13:18:41 +0300

    Add multiple pointers pattern challenge

---
+++ src/problem-solving-patterns/multiple-pointers/find-pair.js
@@ -0,0 +1,45 @@
+// Frequency Counter - findPair
+// Given an unsorted array and a number n, find if there exists a pair of elements
+// in the array whose difference is n. This function should return true
+// if the pair exists or false if it does not.
+
+// Solve this with the following requirements:
+// Time Complexity Requirement - O(n log n)
+// Space Complexity Requirement - O(1)
+
+function findPair(arr, num) {
+  arr.sort((a, b) => a - b);
+
+  const numAbs = Math.abs(num);
+  let i = 0;
+  let j = 1;
+
+  while (j < arr.length) {
+    const diffAbs = Math.abs(arr[i] - arr[j]);
+
+    if (diffAbs === numAbs) return true;
+
+    if (diffAbs > numAbs && i === j - 1) {
+      i++;
+      j++;
+    } else if (diffAbs > numAbs) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+
+  return false;
+}
+
+console.log(findPair([6, 1, 4, 10, 2, 4], 2)); // true
+console.log(findPair([8, 6, 2, 4, 1, 0, 2, 5, 13], 1)); // true
+console.log(findPair([4, -2, 3, 10], -6)); // true
+console.log(findPair([6, 1, 4, 10, 2, 4], 22)); // false
+console.log(findPair([], 0)); // false
+console.log(findPair([5, 5], 0)); // true
+console.log(findPair([-4, 4], -8)); // true
+console.log(findPair([-4, 4], 8)); // true
+console.log(findPair([1, 3, 4, 6], -2)); // true
+console.log(findPair([0, 1, 3, 4, 6], -2)); // true
+console.log(findPair([-2, 5, 10, 15, 16], 0)); // false
