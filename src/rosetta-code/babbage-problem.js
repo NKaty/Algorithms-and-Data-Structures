b2a8988d29a15cac73759d3ad61a4b3c339f0e81
NKaty <nd.katy@gmail.com>
2019-06-24 20:34:32 +0300

    Add rosetta code problem - Babbage problem

---
+++ src/rosetta-code/babbage-problem.js
@@ -0,0 +1,17 @@
+// Charles Babbage, looking ahead to the sorts of problems his
+// Analytical Engine would be able to solve, gave this example:
+// What is the smallest positive integer whose square ends in the digits 269,696?
+// He thought the answer might be 99,736, whose square is 9,947,269,696; but he couldn't be certain.
+// The task is to find out if Babbage had the right answer.
+// Implement a function to return the lowest integer that satisfies the Babbage problem.
+
+function babbage() {
+  const endDigits = 269696;
+  let num = Math.ceil(Math.sqrt(endDigits));
+
+  for (; num * num % 1000000 !== endDigits; num += 2) {}
+
+  return num;
+}
+
+console.log(babbage()); // 25264
