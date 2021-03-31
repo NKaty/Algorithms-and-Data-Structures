c69e00deffc08e35a22f316c4f6c547137009514
NKaty <nd.katy@gmail.com>
2021-03-31 21:15:02 +0300

    Add bit manipulation problem - single number

---
+++ src/bit-manipulation/single-number.js
@@ -0,0 +1,20 @@
+// Given a non-empty array of integers nums,
+// every element appears twice except for one. Find that single one.
+// Could you implement a solution with a linear runtime complexity
+// and without using extra memory?
+
+// Constraints:
+// 1 <= nums.length <= 3 * 104
+// -3 * 104 <= nums[i] <= 3 * 104
+// Each element in the array appears twice except for one element
+// which appears only once.
+
+const singleNumber = (nums) => {
+  if (!nums.length) throw new Error('The array is empty');
+
+  return nums.reduce((singleNum, num) => singleNum ^ num, 0);
+};
+
+console.log(singleNumber([4, 1, 2, 1, 2])); // 4
+console.log(singleNumber([-1, 10, 2, 7, 5, -100, 7, -100, -1, 5, 45, 11, 45, 10, 2])); // 11
+console.log(singleNumber([5])); // 5
