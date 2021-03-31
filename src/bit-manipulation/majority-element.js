af38e052eeb1e7f2a63a3ca63816d8d0a2f07ba5
NKaty <nd.katy@gmail.com>
2021-03-31 22:44:32 +0300

    Add bit manipulation problem - majority element

---
+++ src/bit-manipulation/majority-element.js
@@ -0,0 +1,32 @@
+// Given an array nums of size n, return the majority element.
+// The majority element is the element that appears more than ⌊n / 2⌋ times.
+// You may assume that the majority element always exists in the array.
+// Could you solve the problem in linear time and in O(1) space?
+
+// Constraints:
+// n == nums.length
+// 1 <= n <= 5 * 10 ^ 4
+// -2 ^ 31 <= nums[i] <= 2 ^ 31 - 1
+
+const findMajorityElement = (nums) => {
+  if (!nums.length) throw new Error('The array is empty');
+
+  let majorityElement = 0;
+
+  for (let i = 0; i < 32; i++) {
+    const mask = 1 << i;
+    let bitCount = 0;
+
+    for (const num of nums) {
+      if (num & mask) bitCount++;
+    }
+
+    if (bitCount > nums.length / 2) majorityElement |= mask;
+  }
+
+  return majorityElement;
+};
+
+console.log(findMajorityElement([2, 2, 1, 1, 1, 2, 2])); // 2
+console.log(findMajorityElement([7, 7, -5, 7, -5, 1, -5, 7, -5, -5, 7, 7, 7, 7, 7, 7])); // 7
+console.log(findMajorityElement([4])); // 4
