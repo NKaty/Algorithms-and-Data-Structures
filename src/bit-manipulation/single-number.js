3a8b2eb3675399ae31111baecfc4f90f912f13aa
NKaty <nd.katy@gmail.com>
2021-03-31 22:52:39 +0300

    Fix comments

--- src/bit-manipulation/single-number.js
+++ src/bit-manipulation/single-number.js
@@ -4,8 +4,8 @@
 // and without using extra memory?
 
 // Constraints:
-// 1 <= nums.length <= 3 * 104
-// -3 * 104 <= nums[i] <= 3 * 104
+// 1 <= nums.length <= 3 * 10 ^ 4
+// -3 * 10 ^ 4 <= nums[i] <= 3 * 10 ^ 4
 // Each element in the array appears twice except for one element
 // which appears only once.
 
