f9441f2ba984afa7445629a37f34b8a3d91ec795
Ekaterina Naida <nd.katy@gmail.com>
2023-11-08 22:00:01 +0400

    Add js related problem - sleep implementation

---
+++ src/js-related-problems/sleep.js
@@ -0,0 +1,16 @@
+// Given a positive integer millis,
+// write an asynchronous function that sleeps for millis milliseconds.
+// It can resolve any value.
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), millis);
+  });
+}
+
+const start = Date.now();
+sleep(100).then(() => console.log(Date.now() - start)); // ~100
