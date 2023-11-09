989742d742ad0828f530734efb1d2c43d190f79f
Ekaterina Naida <nd.katy@gmail.com>
2023-11-09 18:25:24 +0400

    Refactor js related problem - debounce

--- src/js-related-problems/debounce.js
+++ src/js-related-problems/debounce.js
@@ -8,6 +8,10 @@
 // would be executed at 150ms. If instead t = 35ms, The 1st call would be cancelled,
 // the 2nd would be executed at 95ms, and the 3rd would be executed at 135ms.
 
+// In the debouncing technique, no matter how many times the user fires the event,
+// the attached function will be executed only after the specified time
+// once the user stops firing the event.
+
 /**
  * @param {Function} fn
  * @param {number} t milliseconds
@@ -16,14 +20,55 @@
 const debounce = function (fn, t) {
   let timer;
   return (...args) => {
+    const context = this;
     if (timer) {
       clearTimeout(timer);
+      timer = undefined;
     }
-    timer = setTimeout(fn, t, ...args);
+    timer = setTimeout(() => {
+      fn.apply(context, args);
+    }, t);
   };
 };
 
-const log = debounce(console.log, 1000);
-log('Hello'); // cancelled
-log('Hello'); // cancelled
-log('Hello'); // Logged at t=1000ms
+const debounceImmediate = function (fn, t, immediate = false) {
+  let timer;
+  return function (...args) {
+    const callNow = immediate && !timer;
+    const context = this;
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      if (!immediate) {
+        fn.apply(context, args);
+        timer = undefined;
+      }
+    }, t);
+    if (callNow) {
+      fn.apply(context, args);
+    }
+  };
+};
+
+const imitateCalls = function (fn) {
+  let i = 0;
+  fn(`Hello${++i}`);
+  const timer = setInterval(function () {
+    if (i >= 9) {
+      clearInterval(timer);
+    }
+    fn(`Hello${++i}`);
+  }, 100);
+};
+
+const start = Date.now();
+imitateCalls(debounce((str) => {
+  console.log('Case debounce:', str, Date.now() - start);
+}, 300)); // Hello10 logged at t = ~1200ms
+imitateCalls(debounceImmediate((str) => {
+  console.log('Case debounce immediate - false:', str, Date.now() - start);
+}, 300)); // Hello10 logged at t = ~1200ms
+imitateCalls(debounceImmediate((str) => {
+  console.log('Case debounce immediate - true:', str, Date.now() - start);
+}, 300, true)); // Hello1 logged at t = ~0ms
